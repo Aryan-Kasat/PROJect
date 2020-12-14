d143390689df3bf7e00ef37437f92696350c4332
Aryan-Kasat <72985192+Aryan-Kasat@users.noreply.github.com>
2020-12-14 09:31:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function getParagraph1(){
+    var inputs= [];
+    for(var i=1;i<=5;i++){
+      inputs.push(document.getElementById("input_1 +1").value;
+    }
+    inputs.join(".");
+      document.getElementById("showParagraph1").innerHTML= inputs.join(".");
